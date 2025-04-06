e027a6ac4ced2569b6c0d340fb9bc26524f25892
Jason Clarence <70305325+nerdylive123@users.noreply.github.com>
2025-04-06 23:19:58 +0800

    feat: add gguf model support
    https://docs.vllm.ai/en/latest/features/quantization/gguf.html
    notes: should use one model only, no split files

--- src/download_model.py
+++ src/download_model.py
@@ -8,7 +8,7 @@ from utils import timer_decorator
 
 BASE_DIR = "/" 
 TOKENIZER_PATTERNS = [["*.json", "tokenizer*"]]
-MODEL_PATTERNS = [["*.safetensors"], ["*.bin"], ["*.pt"]]
+MODEL_PATTERNS = [["*.safetensors"], ["*.bin"], ["*.pt"], ["*.gguf"]]
 
 def setup_env():
     if os.getenv("TESTING_DOWNLOAD") == "1":
